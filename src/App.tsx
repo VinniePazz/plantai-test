20cb5aa83b92f210b832b37ea76848f9a8811624
Dima Smakouz <dimasmakouz@gmail.com>
2020-09-02 22:05:58 +0300

    Add husky and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return <div>Root</div>;
+  return <div>Root</div>
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-};
-
-if (process.env.NODE_ENV !== 'production' && (module as any).hot) {
-  (module as any).hot.accept('./App', renderApp);
+  ReactDOM.render(<App />, document.getElementById('root'))
 }
 
-renderApp();
+if (process.env.NODE_ENV !== 'production' && (module as any).hot) {
+  ;(module as any).hot.accept('./App', renderApp)
+}
+
+renderApp()
