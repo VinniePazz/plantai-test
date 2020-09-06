959e8730f24a0788b6efb34bd0d7bc2c5c0bd43a
Dima Smakouz <dimasmakouz@gmail.com>
2020-09-07 02:26:20 +0300

    Add menu to desktop nav

--- src/components/sections/Header/Navigation.tsx
+++ src/components/sections/Header/Navigation.tsx
@@ -15,12 +15,18 @@ const Navigation = () => {
   // useContext in Navigation component
   const navigation = useMemo(() => {
     const deskTopNav = (
-      <nav className={css.mobileNav}>
+      <nav className={css.navWrapper}>
         <div className={css.logo}>
           <button>
             <span>The</span> <span>Sill</span>
           </button>
         </div>
+        <ul className={css.menu}>
+          <li>Shop</li>
+          <li>Best Sellers</li>
+          <li>Our Summer Picks</li>
+          <li>Online workshops</li>
+        </ul>
         <div className={css.cartLink}>
           <button onClick={toggleCart}>Cart ({itemsInCart})</button>
         </div>
@@ -28,7 +34,7 @@ const Navigation = () => {
     )
 
     const mobileNav = (
-      <nav className={css.mobileNav}>
+      <nav className={css.navWrapper}>
         <div className={css.burgerContainer}>
           <button className={css.burger}>
             <span />
